c87d56c8d32a5d9cfa27a6f62fb270b7018600b2
sselvaggi <sergio.selvaggi@gmail.com>
2016-05-30 19:17:19 -0300

    [RomanNumber] Refactor (partial commit)

--- roman_numbers/RomanNumber.py
+++ roman_numbers/RomanNumber.py
@@ -1,22 +1,53 @@
 class RomanNumberHelper:
+
+    def __init__(self):
+        self.symbols = [['I','V'],['X','L'],['C','D'],['M','?']]
+        self.digits = range(0,9)
+
+    def symbol_by_digit_and_exp(self, digit, exp):
+        #validate  0 < digit < 10
+        if(digit == 1):
+            return self.symbols[exp][0]
+        elif(digit == 2):
+            return self.symbols[exp][0]+self.symbols[exp][0]
+        elif(digit == 3):
+            return self.symbols[exp][0]+self.symbols[exp][0]+self.symbols[exp][0]
+        elif(digit == 4):
+            return self.symbols[exp][0]+self.symbols[exp][1]
+        elif(digit == 5):
+            return self.symbols[exp][1]
+        elif(digit == 6):
+            return self.symbols[exp][1]+self.symbols[exp][0]
+        elif(digit == 7):
+            return self.symbols[exp][1]+self.symbols[exp][0]+self.symbols[exp][0]
+        elif(digit == 8):
+            return self.symbols[exp][1]+self.symbols[exp][0]+self.symbols[exp][0]+self.symbols[exp][0]
+        elif(digit == 9):
+            return self.symbols[exp][0]+self.symbols[exp+1][0]
     def to_roman(self, integer):
-    	if(integer == 1):
-            return "I"
-        elif(integer == 2):
-            return "II"
-        elif(integer == 3):
-            return "III"
-        elif(integer == 4):
-            return "IV"
-        elif(integer == 5):
-            return "V"
-        elif(integer == 6):
-            return "VI"
-        elif(integer == 7):
-        	return "VII"
-        elif(integer == 8):
-        	return "VIII"
-        elif(integer == 9):
-        	return "IX"
-        elif(integer == 10):
-        	return "X"
+        result = ''
+        for exp in range(self.get_max_decimal_exponent(integer),0):
+            digit = self.get_digit_by_exp(integer,exp)
+            result += self.symbol_by_digit_and_exp(digit,exp)
+        return result 
+    
+    def get_max_decimal_exponent(self, integer):
+        exp = 0
+        while((10**exp) < integer):
+            exp+=1
+        return exp
+
+    # integer=1  exp=0 return 1
+    # integer=2  exp=0 return 2
+    # integer=10 exp=0 return 0
+    # integer=10 exp=1 return 1
+    # integer=20 exp=1 return 2
+    # integer=10 exp=2 return 0
+    def get_digit_by_exp(self, integer,exp):
+        digit = 0
+        max_exp = self.get_max_decimal_exponent(integer)
+        mod = integer % (10**(exp+1))
+        for i in range(0, 10):
+            if (i*(10**exp) <= mod):
+                digit = i
+        return digit
--- roman_numbers/test.py
+++ roman_numbers/test.py
@@ -4,6 +4,7 @@
 # for 2 show II
 # for 1834 show MCCMXXXIV
 import unittest
+import pdb
 from RomanNumber import RomanNumberHelper
 
 
@@ -40,6 +41,30 @@ class TestRomanNumber(unittest.TestCase):
 
     def test_number_10(self):
         self.assertEqual(self.helper.to_roman(10), 'X')
+
+    def test_number_11(self):
+        self.assertEqual(self.helper.to_roman(11), 'XI')
+
+    def test_get_max_decimal_exponent(self):
+        self.assertEqual(self.helper.get_max_decimal_exponent(1), 0)
+        self.assertEqual(self.helper.get_max_decimal_exponent(5), 0)
+        self.assertEqual(self.helper.get_max_decimal_exponent(10), 1)
+        self.assertEqual(self.helper.get_max_decimal_exponent(90), 1)
+        self.assertEqual(self.helper.get_max_decimal_exponent(555), 2)
+
+    def test_get_digit_by_exp(self):
+    # integer=1  exp=0 return 1
+    # integer=2  exp=0 return 2
+    # integer=10 exp=0 return 0
+    # integer=10 exp=1 return 1
+    # integer=20 exp=1 return 2
+    # integer=10 exp=2 return 0
+        self.assertEqual(self.helper.get_digit_by_exp(0,0), 0)
+        self.assertEqual(self.helper.get_digit_by_exp(1,0), 1)
+        self.assertEqual(self.helper.get_digit_by_exp(2,0), 2)
+        self.assertEqual(self.helper.get_digit_by_exp(22,1), 2)
+        self.assertEqual(self.helper.get_digit_by_exp(256,1), 5)
     
+
 if __name__ == '__main__':
     unittest.main()
